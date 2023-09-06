70b3dc3aa132ef8753e226fac665f59669dbb45e
Rahman1234562 <126483990+Rahman1234562@users.noreply.github.com>
2023-09-06 12:13:50 -0700

    some changed in file

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,25 +1,37 @@
 import React, { useState } from "react";
 
 export default function TextForm(props) {
-    const handleUpClick = () => {
-        // console.log("upper was clicked" + text);
-       let newText = text.toUpperCase();
-       setText(newText);
-    }
+  const handleUpClick = () => {
+    // console.log("upper was clicked" + text);
+    let newText = text.toUpperCase();
+    setText(newText);
+  };
 
-    const handleOnChange = (event) => {
-        // console.log("on change");
-        setText(event.target.value)
-    }
-    const[text, setText] = useState("use the text");
+  const handleLuClick = () => {
+    // console.log("upper was clicked" + text);
+    let newText = text.toLowerCase();
+    setText(newText);
+  };
+  const handleClearClick = () => {
+    // console.log("upper was clicked" + text);
+    let newText = ' ';
+    setText(newText);
+  };
+
+
+  const handleOnChange = (event) => {
+    // console.log("on change");
+    setText(event.target.value);
+  };
+  const [text, setText] = useState("");
+  
 
   return (
     <div>
-       <h1>{props.heading}</h1>
+      <h1>{props.heading}</h1>
 
-      <div className="mb-3">
-        <label htmlFor="textBox" className="form-label">
-        </label>
+      <div className="container mb-3">
+        <label htmlFor="textBox" className="form-label"></label>
         <textarea
           className="form-control"
           value={text}
@@ -27,7 +39,24 @@ export default function TextForm(props) {
           id="textBox"
           rows="8"
         ></textarea>
-        <button className="btn btn-primary my-2 " onClick={handleUpClick}>tap to upperCase</button>
+        <button className="btn btn-primary my-2 mx-1" onClick={handleUpClick}>
+          tap to upperCase
+        </button>
+        <button className="btn btn-primary my-2 mx-1" onClick={handleLuClick}>
+          tap to lowerCase
+        </button>
+        <button className="btn btn-primary my-2 mx-1" onClick={handleClearClick}>
+        clear text
+        </button>
+        
+      </div>
+      <div className="container">
+        <h2>your text Proview</h2>
+        <p>{text.split(" ").length} word and {text.length} character</p>
+        <p>{0.008 * text.split(" ").length} Minute read</p>
+        <p>{text.split( " ").length -1} space count</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
       </div>
     </div>
   );
